ca201a66ab1bd419fc479bae2738cca9326e2ee9
Peter Froehlich <peter.hans.froehlich@gmail.com>
2009-12-13 18:37:28 -0500

    GetError and CaptureDevice.

--- core.go
+++ core.go
@@ -6,7 +6,12 @@ package openal
 
 /*
 #include <stdlib.h>
+
+// It's sad but the OpenAL C API uses lots and lots of typedefs
+// that require wrapper function to make things work with cgo.
+
 #include <AL/al.h>
+
 //AL_API void AL_APIENTRY alGenSources( ALsizei n, ALuint* sources );
 //AL_API void AL_APIENTRY alDeleteSources( ALsizei n, const ALuint* sources );
 //AL_API void AL_APIENTRY alGenBuffers( ALsizei n, ALuint* buffers );
@@ -16,57 +21,121 @@ package openal
 //AL_API void AL_APIENTRY alSourceStop( ALuint sid );
 //AL_API void AL_APIENTRY alSourceRewind( ALuint sid );
 //AL_API void AL_APIENTRY alSourcePause( ALuint sid );
-// AL_API void AL_APIENTRY alSourceQueueBuffers( ALuint sid, ALsizei numEntries, const ALuint *bids );
+//AL_API void AL_APIENTRY alSourceQueueBuffers( ALuint sid, ALsizei numEntries, const ALuint *bids );
 //AL_API void AL_APIENTRY alSourceUnqueueBuffers( ALuint sid, ALsizei numEntries, ALuint *bids );
 //AL_API ALenum AL_APIENTRY alGetError( void );
 //AL_API void AL_APIENTRY alSourcei( ALuint sid, ALenum param, ALint value );
 //AL_API void AL_APIENTRY alGetSourcei( ALuint sid,  ALenum param, ALint* value );
+
 #include <AL/alc.h>
-//ALC_API ALCdevice *     ALC_APIENTRY alcOpenDevice( const ALCchar *devicename );
+
 ALCdevice *walcOpenDevice(const char *devicename) {
 	return alcOpenDevice(devicename);
 }
-//ALC_API ALCboolean      ALC_APIENTRY alcCloseDevice( ALCdevice *device );
+
 int walcCloseDevice(ALCdevice *device) {
 	return alcCloseDevice(device);
 }
-//ALC_API ALCenum         ALC_APIENTRY alcGetError( ALCdevice *device );
+
+int walcGetError(ALCdevice *device) {
+	return alcGetError(device);
+}
+
 //ALC_API ALCcontext *    ALC_APIENTRY alcCreateContext( ALCdevice *device, const ALCint* attrlist );
 //ALC_API ALCboolean      ALC_APIENTRY alcMakeContextCurrent( ALCcontext *context );
 //ALC_API void            ALC_APIENTRY alcDestroyContext( ALCcontext *context );
-//ALC_API ALCdevice*      ALC_APIENTRY alcCaptureOpenDevice( const ALCchar *devicename, ALCuint frequency, ALCenum format, ALCsizei buffersize );
-//ALC_API ALCboolean      ALC_APIENTRY alcCaptureCloseDevice( ALCdevice *device );
+
+ALCdevice *walcCaptureOpenDevice(const char *devicename, int frequency, int format, int buffersize) {
+	return alcCaptureOpenDevice(devicename, frequency, format, buffersize);
+}
+
+int walcCaptureCloseDevice(ALCdevice *device) {
+	return alcCaptureCloseDevice(device);
+}
+
 //ALC_API void            ALC_APIENTRY alcCaptureStart( ALCdevice *device );
 //ALC_API void            ALC_APIENTRY alcCaptureStop( ALCdevice *device );
 //ALC_API void            ALC_APIENTRY alcCaptureSamples( ALCdevice *device, ALCvoid *buffer, ALCsizei samples );
 //ALC_API void            ALC_APIENTRY alcGetIntegerv( ALCdevice *device, ALCenum param, ALCsizei size, ALCint *data );
-#include <AL/alext.h>
-#include <AL/alut.h>
+
+//#include <AL/alext.h>
+//#include <AL/alut.h>
 */
 import "C"
+import "unsafe"
 
-import (
-	"unsafe";
+// All of the following are eventually going to be
+// private to the Go OpenAL binding. For now I am
+// just playing around, so they are public. Not for
+// long I hope. :-D
+
+// what Device.GetError returns
+const (
+	AlcNoError = 0;
+	AlcInvalidDevice =0xA001;
+	AlcInvalidContext = 0xA002;
+	AlcInvalidEnum = 0xA003;
+	AlcInvalidValue = 0xA004;
+	AlcOutOfMemory = 0xA005;
+)
+
+// format for CaptureOpenDevice
+const (
+	AlFormatMono8 = 0x1100;
+	AlFormatMono16 = 0x1101;
+	AlFormatStereo8 = 0x1102;
+	AlFormatStereo16 = 0x1103;
 )
 
 type Device struct {
 	handle *C.ALCdevice;
 }
 
-func OpenDevice(name string) *Device {
+func OpenDevice(name string) (device *Device) {
 	p := C.CString(name);
 	h := C.walcOpenDevice(p);
 	C.free(unsafe.Pointer(p));
 
 	if h == nil {
-		return nil;
+		return;
 	}
 
-	d := new(Device);
-	d.handle = h;
-	return d;
+	device = new(Device);
+	device.handle = h;
+	return;
 }
 
-func CloseDevice(device *Device) bool {
-	return C.walcCloseDevice(device.handle) != 0;
+func (self *Device) CloseDevice() bool {
+	return C.walcCloseDevice(self.handle) != 0;
 }
+
+func (self *Device) GetError() int {
+	return int(C.walcGetError(self.handle));
+}
+
+type CaptureDevice struct {
+	handle *C.ALCdevice;
+}
+
+func CaptureOpenDevice(name string, freq int, format int, size int) (device *CaptureDevice) {
+	p := C.CString(name);
+	h := C.walcCaptureOpenDevice(p, C.int(freq), C.int(format), C.int(size));
+	C.free(unsafe.Pointer(p));
+
+	if h == nil {
+		return;
+	}
+
+	device = new(CaptureDevice);
+	device.handle = h;
+	return;
+}
+
+func (self *CaptureDevice) CaptureCloseDevice() bool {
+	return C.walcCaptureCloseDevice(self.handle) != 0;
+}
+
+func (self *CaptureDevice) GetError() int {
+	return int(C.walcGetError(self.handle));
+}
+
--- example.go
+++ example.go
@@ -6,6 +6,15 @@ import "fmt"
 func main() {
 	dev := openal.OpenDevice("");
 	fmt.Printf("dev: %s\n", dev);
-	ok := openal.CloseDevice(dev);
+	err := dev.GetError();
+	fmt.Printf("err: %s\n", err);
+	ok := dev.CloseDevice();
+	fmt.Printf("ok: %s\n", ok);
+
+	mic := openal.CaptureOpenDevice("", 16000, openal.AlFormatMono8, 16000);
+	fmt.Printf("mic: %s\n", mic);
+	err = mic.GetError();
+	fmt.Printf("err: %s\n", err);
+	ok = mic.CaptureCloseDevice();
 	fmt.Printf("ok: %s\n", ok);
 }
