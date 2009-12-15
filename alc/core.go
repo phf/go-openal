b76ece1833a3a5a8bf3c3fb9b0aa6a5b09d830c8
Peter Froehlich <peter.hans.froehlich@gmail.com>
2009-12-15 02:13:50 -0500

    Back where we were with ALC, just cuter.

--- alc/core.go
+++ alc/core.go
@@ -39,7 +39,10 @@ ALCboolean alcIsExtensionPresent( ALCdevice *device, const ALCchar *extname );
 void *alcGetProcAddress( ALCdevice *device, const ALCchar *funcname );
 ALCenum alcGetEnumValue( ALCdevice *device, const ALCchar *enumname );
 const ALCchar *alcGetString( ALCdevice *device, ALCenum param );
-void alcGetIntegerv( ALCdevice *device, ALCenum param, ALCsizei size, ALCint *data );
+//void alcGetIntegerv( ALCdevice *device, ALCenum param, ALCsizei size, ALCint *data );
+void walcGetIntegerv(ALCdevice *device, ALCenum param, ALCsizei size, void *data) {
+	alcGetIntegerv(device, param, size, data);
+}
 // ALCdevice *alcCaptureOpenDevice( const ALCchar *devicename, ALCuint frequency, ALCenum format, ALCsizei buffersize );
 ALCdevice *walcCaptureOpenDevice(const char *devicename, ALCuint frequency, ALCenum format, ALCsizei buffersize) {
 	return alcCaptureOpenDevice(devicename, frequency, format, buffersize);
@@ -48,9 +51,22 @@ ALCboolean alcCaptureCloseDevice( ALCdevice *device );
 void alcCaptureStart( ALCdevice *device );
 void alcCaptureStop( ALCdevice *device );
 void alcCaptureSamples( ALCdevice *device, ALCvoid *buffer, ALCsizei samples );
+
+// For convenience we offer "singular" versions of the following
+// calls as well, which require different wrappers if we want to
+// be efficient. The main reason for "singular" versions is that
+// Go doesn't allow us to treat a variable as an array.
+
+ALCint walcGetInteger(ALCdevice *device, ALCenum param) {
+	ALCint result;
+	alcGetIntegerv(device, param, 1, &result);
+	return result;
+}
 */
 import "C"
 import "unsafe"
+
+import "openal/al"
 
 // Error codes returned by Device.GetError().
 const (
@@ -61,6 +77,41 @@ const (
 	InvalidValue = 0xA004;
 	OutOfMemory = 0xA005;
 )
+
+const (
+	Frequency = 0x1007; // int Hz
+	Refresh = 0x1008; // int Hz
+	Sync = 0x1009; // bool
+	MonoSources = 0x1010; // int
+	StereoSources = 0x1011; // int
+)
+
+// The Specifier string for default device?
+const (
+	DefaultDeviceSpecifier = 0x1004;
+	DeviceSpecifier = 0x1005;
+	Extensions = 0x1006;
+)
+
+// ?
+const (
+	MajorVersion = 0x1000;
+	MinorVersion = 0x1001;
+)
+
+// ?
+const (
+	AttributesSize = 0x1002;
+	AllAttributes = 0x1003;
+)
+
+// Capture extension
+const (
+	CaptureDeviceSpecifier = 0x310;
+	CaptureDefaultDeviceSpecifier = 0x311;
+	CaptureSamples = 0x312;
+)
+
 
 type Device struct {
 	handle *C.ALCdevice;
@@ -92,8 +143,22 @@ func (self Device) CreateContext() Context {
 	return Context{C.alcCreateContext(self.handle, nil)};
 }
 
+func (self Device) GetIntegerv(param uint32, size uint32) (result []int32) {
+	result = make([]int32, size);
+	C.walcGetIntegerv(self.handle, C.ALCenum(param), C.ALCsizei(size), unsafe.Pointer(&result[0]));
+	return;
+}
+
+func (self Device) GetInteger(param uint32) int32 {
+	return int32(C.walcGetInteger(self.handle, C.ALCenum(param)));
+}
+
+
+
+
 type CaptureDevice struct {
 	Device;
+	sampleSize uint32;
 }
 
 func CaptureOpenDevice(name string, freq uint32, format uint32, size uint32) (device CaptureDevice) {
@@ -102,7 +167,8 @@ func CaptureOpenDevice(name string, freq uint32, format uint32, size uint32) (de
 	p := C.CString(name);
 	h := C.walcCaptureOpenDevice(p, C.ALCuint(freq), C.ALCenum(format), C.ALCsizei(size));
 	C.free(unsafe.Pointer(p));
-	return CaptureDevice{Device{h}};
+	s := map[uint32]uint32{al.FormatMono8: 1, al.FormatMono16: 2, al.FormatStereo8: 2, al.FormatStereo16: 4}[format];
+	return CaptureDevice{Device{h},s};
 }
 
 func (self CaptureDevice) CloseDevice() bool {
@@ -121,13 +187,15 @@ func (self CaptureDevice) CaptureStop() {
 	C.alcCaptureStop(self.handle);
 }
 
-
+func (self *CaptureDevice) CaptureSamples(size uint32) (data []byte) {
+	data = make([]byte, size * self.sampleSize);
+	C.alcCaptureSamples(self.handle, unsafe.Pointer(&data[0]), C.ALCsizei(size));
+	return;
+}
 
 type Context struct {
 	handle *C.ALCcontext;
 }
-
-
 
 func (self Context) MakeContextCurrent() bool {
 	return C.alcMakeContextCurrent(self.handle) != 0;
@@ -136,7 +204,5 @@ func (self Context) MakeContextCurrent() bool {
 func (self Context) DestroyContext() {
 	C.alcDestroyContext(self.handle);
 	self.handle = nil;
-	// XXX: there used to be a alcDestroyContext() that
-	// returned something, but our alc.h doesn't list
-	// that one... Hmmm...
 }
+
