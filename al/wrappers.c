fffd7beedd5b69f0df6413ff1bd5411e92e51338
Peter Froehlich <peter.hans.froehlich@gmail.com>
2009-12-15 20:11:37 -0500

    All functions I can wrap are wrapped. Done!

--- al/wrappers.c
+++ al/wrappers.c
@@ -7,212 +7,149 @@
 // to grok them. So there's a lot more C code here than I would
 // like...
 
-// I keep all the al.h prototypes here for now, for reference.
-// They'll go away eventually. Those commented out are already
-// accessible from Go.
-
-// void alEnable( ALenum capability );
-// void alDisable( ALenum capability ); 
-// ALboolean alIsEnabled( ALenum capability ); 
-// const ALchar* alGetString( ALenum param );
 const char *walGetString(ALenum param) {
 	return alGetString(param);
 }
-// void alGetBooleanv( ALenum param, ALboolean* data );
+
 void walGetBooleanv(ALenum param, void* data) {
 	alGetBooleanv(param, data);
 }
-// void alGetIntegerv( ALenum param, ALint* data );
 void walGetIntegerv(ALenum param, void* data) {
 	alGetIntegerv(param, data);
 }
-// void alGetFloatv( ALenum param, ALfloat* data );
 void walGetFloatv(ALenum param, void* data) {
 	alGetFloatv(param, data);
 }
-// void alGetDoublev( ALenum param, ALdouble* data );
 void walGetDoublev(ALenum param, void* data) {
 	alGetDoublev(param, data);
 }
-// ALboolean alGetBoolean( ALenum param );
-// ALint alGetInteger( ALenum param );
-// ALfloat alGetFloat( ALenum param );
-// ALdouble alGetDouble( ALenum param );
-// ALenum alGetError( void );
-ALboolean alIsExtensionPresent( const ALchar* extname );
-void* alGetProcAddress( const ALchar* fname );
-ALenum alGetEnumValue( const ALchar* ename );
-// void alListenerf( ALenum param, ALfloat value );
-// void alListener3f( ALenum param, ALfloat value1, ALfloat value2, ALfloat value3 );
-// void alListenerfv( ALenum param, const ALfloat* values ); 
+
+// We don't define wrappers for these because we have
+// no clue how to make Go grok C function pointers at
+// runtime. So for now, OpenAL extensions can not be
+// used from Go. If you have an idea for how to make
+// it work, be sure to email! I suspect we'd need a
+// mechanism for generating cgo-style stubs at runtime,
+// sounds like work.
+//
+// ALboolean alIsExtensionPresent( const ALchar* extname );
+// void* alGetProcAddress( const ALchar* fname );
+// ALenum alGetEnumValue( const ALchar* ename );
+
 void walListenerfv(ALenum param, const void* values) {
 	alListenerfv(param, values);
 }
-// void alListeneri( ALenum param, ALint value );
-// void alListener3i( ALenum param, ALint value1, ALint value2, ALint value3 );
-// void alListeneriv( ALenum param, const ALint* values );
 void walListeneriv(ALenum param, const void* values) {
 	alListeneriv(param, values);
 }
-// void alGetListenerf( ALenum param, ALfloat* value );
 ALfloat walGetListenerf(ALenum param) {
 	ALfloat result;
 	alGetListenerf(param, &result);
 	return result;
 }
-// void alGetListener3f( ALenum param, ALfloat *value1, ALfloat *value2, ALfloat *value3 );
 void walGetListener3f(ALenum param, void *value1, void *value2, void *value3) {
 	alGetListener3f(param, value1, value2, value3);
 }
-// void alGetListenerfv( ALenum param, ALfloat* values );
 void walGetListenerfv(ALenum param, void* values) {
 	alGetListenerfv(param, values);
 }
-// void alGetListeneri( ALenum param, ALint* value );
 ALint walGetListeneri(ALenum param) {
 	ALint result;
 	alGetListeneri(param, &result);
 	return result;
 }
-// void alGetListener3i( ALenum param, ALint *value1, ALint *value2, ALint *value3 );
 void walGetListener3i(ALenum param, void *value1, void *value2, void *value3) {
 	alGetListener3i(param, value1, value2, value3);
 }
-// void alGetListeneriv( ALenum param, ALint* values );
 void walGetListeneriv(ALenum param, void* values) {
 	alGetListeneriv(param, values);
 }
-//void alGenSources( ALsizei n, ALuint* sources ); 
 void walGenSources(ALsizei n, void *sources) {
 	alGenSources(n, sources);
 }
-//void alDeleteSources( ALsizei n, const ALuint* sources );
 void walDeleteSources(ALsizei n, const void *sources) {
 	alDeleteSources(n, sources);
 }
-// ALboolean alIsSource( ALuint sid ); 
-// void alSourcef( ALuint sid, ALenum param, ALfloat value ); 
-// void alSource3f( ALuint sid, ALenum param, ALfloat value1, ALfloat value2, ALfloat value3 );
-// void alSourcefv( ALuint sid, ALenum param, const ALfloat* values ); 
 void walSourcefv(ALuint sid, ALenum param, const void* values) {
 	alSourcefv(sid, param, values);
 }
-// void alSourcei( ALuint sid, ALenum param, ALint value ); 
-// void alSource3i( ALuint sid, ALenum param, ALint value1, ALint value2, ALint value3 );
-// void alSourceiv( ALuint sid, ALenum param, const ALint* values );
 void walSourceiv(ALuint sid, ALenum param, const void* values) {
 	alSourceiv(sid, param, values);
 }
-// void alGetSourcef( ALuint sid, ALenum param, ALfloat* value );
 ALfloat walGetSourcef(ALuint sid, ALenum param) {
 	ALfloat result;
 	alGetSourcef(sid, param, &result);
 	return result;
 }
-// void alGetSource3f( ALuint sid, ALenum param, ALfloat* value1, ALfloat* value2, ALfloat* value3);
 void walGetSource3f(ALuint sid, ALenum param, void *value1, void *value2, void *value3) {
 	alGetSource3f(sid, param, value1, value2, value3);
 }
-// void alGetSourcefv( ALuint sid, ALenum param, ALfloat* values );
 void walGetSourcefv(ALuint sid, ALenum param, void* values) {
 	alGetSourcefv(sid, param, values);
 }
-// void alGetSourcei( ALuint sid,  ALenum param, ALint* value );
 ALint walGetSourcei(ALuint sid, ALenum param) {
 	ALint result;
 	alGetSourcei(sid, param, &result);
 	return result;
 }
-// void alGetSource3i( ALuint sid, ALenum param, ALint* value1, ALint* value2, ALint* value3);
 void walGetSource3i(ALuint sid, ALenum param, void *value1, void *value2, void *value3) {
 	alGetSource3i(sid, param, value1, value2, value3);
 }
-// void alGetSourceiv( ALuint sid,  ALenum param, ALint* values );
 void walGetSourceiv(ALuint sid, ALenum param, void* values) {
 	alGetSourceiv(sid, param, values);
 }
-// void alSourcePlayv( ALsizei ns, const ALuint *sids );
 void walSourcePlayv(ALsizei ns, const void *sids) {
 	alSourcePlayv(ns, sids);
 }
-// void alSourceStopv( ALsizei ns, const ALuint *sids );
 void walSourceStopv(ALsizei ns, const void *sids) {
 	alSourceStopv(ns, sids);
 }
-// void alSourceRewindv( ALsizei ns, const ALuint *sids );
 void walSourceRewindv(ALsizei ns, const void *sids) {
 	alSourceRewindv(ns, sids);
 }
-// void alSourcePausev( ALsizei ns, const ALuint *sids );
 void walSourcePausev(ALsizei ns, const void *sids) {
 	alSourcePausev(ns, sids);
 }
-// void alSourcePlay( ALuint sid );
-// void alSourceStop( ALuint sid );
-// void alSourceRewind( ALuint sid );
-// void alSourcePause( ALuint sid );
-// void alSourceQueueBuffers( ALuint sid, ALsizei numEntries, const ALuint *bids );
 void walSourceQueueBuffers(ALuint sid, ALsizei numEntries, const void *bids) {
 	alSourceQueueBuffers(sid, numEntries, bids);
 }
-//void alSourceUnqueueBuffers( ALuint sid, ALsizei numEntries, ALuint *bids );
 void walSourceUnqueueBuffers(ALuint sid, ALsizei numEntries, void *bids) {
 	alSourceUnqueueBuffers(sid, numEntries, bids);
 }
-//void alGenBuffers( ALsizei n, ALuint* buffers );
 void walGenBuffers(ALsizei n, void *buffers) {
 	alGenBuffers(n, buffers);
 }
-//void alDeleteBuffers( ALsizei n, const ALuint* buffers );
 void walDeleteBuffers(ALsizei n, const void *buffers) {
 	alDeleteBuffers(n, buffers);
 }
-// ALboolean alIsBuffer( ALuint bid );
-// void alBufferData( ALuint bid, ALenum format, const ALvoid* data, ALsizei size, ALsizei freq );
-// void alBufferf( ALuint bid, ALenum param, ALfloat value );
-// void alBuffer3f( ALuint bid, ALenum param, ALfloat value1, ALfloat value2, ALfloat value3 );
-// void alBufferfv( ALuint bid, ALenum param, const ALfloat* values );
 void walBufferfv(ALuint bid, ALenum param, const void* values) {
 	alBufferfv(bid, param, values);
 }
-//void alBufferi( ALuint bid, ALenum param, ALint value );
-//void alBuffer3i( ALuint bid, ALenum param, ALint value1, ALint value2, ALint value3 );
-//void alBufferiv( ALuint bid, ALenum param, const ALint* values );
 void walBufferiv(ALuint bid, ALenum param, const void* values) {
 	alBufferiv(bid, param, values);
 }
-// void alGetBufferf( ALuint bid, ALenum param, ALfloat* value );
 ALfloat walGetBufferf(ALuint bid, ALenum param) {
 	ALfloat result;
 	alGetBufferf(bid, param, &result);
 	return result;
 }
-// void alGetBuffer3f( ALuint bid, ALenum param, ALfloat* value1, ALfloat* value2, ALfloat* value3);
 void walGetBuffer3f(ALuint bid, ALenum param, void *value1, void *value2, void *value3) {
 	alGetBuffer3f(bid, param, value1, value2, value3);
 }
-// void alGetBufferfv( ALuint bid, ALenum param, ALfloat* values );
 void walGetBufferfv(ALuint bid, ALenum param, void* values) {
 	alGetBufferfv(bid, param, values);
 }
-// void alGetBufferi( ALuint bid, ALenum param, ALint* value );
 ALint walGetBufferi(ALuint bid, ALenum param) {
 	ALint result;
 	alGetBufferi(bid, param, &result);
 	return result;
 }
-// void alGetBuffer3i( ALuint bid, ALenum param, ALint* value1, ALint* value2, ALint* value3);
 void walGetBuffer3i(ALuint bid, ALenum param, void *value1, void *value2, void *value3) {
 	alGetBuffer3i(bid, param, value1, value2, value3);
 }
-// void alGetBufferiv( ALuint bid, ALenum param, ALint* values );
 void walGetBufferiv(ALuint bid, ALenum param, void* values) {
 	alGetBufferiv(bid, param, values);
 }
-// void alDopplerFactor( ALfloat value );
-// void alDopplerVelocity( ALfloat value );
-// void alSpeedOfSound( ALfloat value );
-// void alDistanceModel( ALenum distanceModel );
 
 // For convenience we offer "singular" versions of the following
 // calls as well, which require different wrappers if we want to
